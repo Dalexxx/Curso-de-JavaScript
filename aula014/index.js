b56f2c37eb600546273d2fbedff8e0b38502dff5
David Alex Souza Santos <davidalex989@gmail.com>
2024-01-15 11:35:49 -0300

    Create index.js

---
+++ aula014/index.js
@@ -0,0 +1,17 @@
+// IEE 754-2008
+let num1 = 0.7;
+let num2 = 0.1;
+
+num1 += num2;
+num1 += num2;
+num1 += num2;
+
+console.log(num1);
+
+// console.log(num1.toString() + num2)
+// console.log(typeof(num1))
+// console.log(num1.toFixed(2))
+// console.log(Number.isNaN(temp));
+// let temp = num1 * 'sarve';
+
+
