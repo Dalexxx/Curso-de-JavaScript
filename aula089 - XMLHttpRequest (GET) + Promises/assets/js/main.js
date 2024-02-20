8b091f15a274454a1795f16b9c24ec8061a02f9e
David Alex Souza Santos <davidalex989@gmail.com>
2024-02-20 10:53:20 -0300

    add

---
+++ aula089 - XMLHttpRequest (GET) + Promises/assets/js/main.js
@@ -0,0 +1,45 @@
+const request = obj => {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(obj.method, obj.url, true);
+        xhr.send();
+
+        xhr.addEventListener('load', () => {
+            if(xhr.status >= 200 && xhr.status < 300) { //entre 200 e 300 é sucesso
+            resolve(xhr.responseText);
+            } else {
+            reject(xhr.statusText);
+            }
+        });  
+    })
+};
+
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if (tag === 'a') {
+        e.preventDefault();
+        loadPage(el);
+    }
+});
+
+async function loadPage(el) {
+    const href = el.getAttribute('href');
+    
+    const objConfig = {
+        method: 'GET',
+        url: href
+    };
+    try{
+        const response = await request(objConfig);
+        loadResult(response)
+    } catch(e) {
+        console.log(e)
+    };
+}
+
+function loadResult(response) {
+    const result = document.querySelector('.resultado');
+    result.innerHTML = response;
+}
